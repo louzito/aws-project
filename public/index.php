0c190e9a379a2edfc2f58ae1e2d0b7e10d9f9027
Lou Zito <zito.lou@gmail.com>
2018-03-29 10:04:33 +0200

    s3 client

--- public/index.php
+++ public/index.php
@@ -7,6 +7,13 @@ function dump($var) {
     echo '</pre>';
 }
 
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+
+dump($s3);
+
 ?>
 
 <h1>AWS PROJECT</h1>
