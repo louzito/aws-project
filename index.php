a7cd0a4b3598361b70d40bda0265d8abeb491714
Lou Zito <zito.lou@gmail.com>
2018-03-29 09:38:57 +0200

    composer

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo '<h1>AWS PROJECT</h1>';
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+function dump($var) {
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+}
+
+?>
+
+<h1>AWS PROJECT</h1>
